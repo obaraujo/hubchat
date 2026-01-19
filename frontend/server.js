6759c5d3a0964c0d97aa8f8b7e9cabb0d6478fa6
obaraujo <viniciusaraujotech@gmail.com>
2026-01-19 14:20:23 -0300

    bug fix server front

--- frontend/server.js
+++ frontend/server.js
@@ -2,9 +2,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 app.get("/*", function (req, res) {
-	res.sendFile(path.join(__dirname, "build", "index.html"));
+	res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
-app.listen(3005);
 
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running on port " + (process.env.PORT || 3000));
+});
