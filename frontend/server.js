d201b83c8c1807fc41a792a91f2d4ba85253ab67
obaraujo <viniciusaraujotech@gmail.com>
2026-01-20 11:35:19 -0300

    load .env

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+require('dotenv').config()
+
 app.use(express.static(path.join(__dirname, "dist")));
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "dist", "index.html"));
