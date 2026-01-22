b60f4840a6667da59e19c7b573aed73cd0445ed3
obaraujo <viniciusaraujotech@gmail.com>
2026-01-22 09:29:04 -0300

    allowed origins

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -45,7 +45,7 @@ app.set("queues", {
   sendScheduledMessages
 });
 
-const allowedOrigins = [process.env.FRONTEND_URL];
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',').map(origin => origin.trim()) : [process.env.FRONTEND_URL];
 
 // Configuração do BullBoard
 if (String(process.env.BULL_BOARD).toLocaleLowerCase() === 'true' && process.env.REDIS_URI_ACK !== '') {
